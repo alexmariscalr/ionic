21adc7ed48f8740442bb2340d0d7ea0b7c0a6c32
Alejandro <alejandro.mariscal.romero@alumnos.upm.es>
2022-03-03 09:54:58 +0100

    Demo Commit

---
+++ myfirstionicproject/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myfirstionicproject',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
